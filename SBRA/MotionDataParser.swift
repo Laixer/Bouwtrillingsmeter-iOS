6cda9932282b420f38662f7bdda73f9845d4592d
Wander Siemers <wandersiemers@me.com>
2018-11-25 19:02:15 +0100

    Deleted storyboard, wrote function to calculate speed in MotionDataParser

--- SBRA/MotionDataParser.swift
+++ SBRA/MotionDataParser.swift
@@ -15,10 +15,11 @@ class MotionDataParser: NSObject {
 	
 	let manager = CMMotionManager()
 	var data = [DataPoint]()
+	var speedValues = [Float]()
 	
 	func startDataCollection(handler: @escaping MotionDataHandler) {
 		if (manager.isDeviceMotionAvailable) {
-			let interval = 0.1
+			let interval = 0.01
 			manager.deviceMotionUpdateInterval = interval
 			var timestamp: TimeInterval = 0.0
 			manager.startDeviceMotionUpdates(to: OperationQueue.main) { (motion, error) in
@@ -29,7 +30,9 @@ class MotionDataParser: NSObject {
 				if let motion = motion {
 					timestamp += interval
 					let dataPoint = DataPoint(acceleration: motion.userAcceleration, rotationRate: motion.rotationRate, timestamp: timestamp)
+					
 					self.data.append(dataPoint)
+					self.updateSpeed()
 					handler(dataPoint, nil)
 					
 				} else {
@@ -38,4 +41,28 @@ class MotionDataParser: NSObject {
 			}
 		}
 	}
+	
+	private func updateSpeed() {
+		var speed = self.speedValues.last ?? 0
+		
+		var vx: Decimal = 0
+		var vy: Decimal = 0
+		var vz: Decimal = 0
+		
+		if let data = data.last {
+			let ax = Decimal(data.acceleration.x)
+			let ay = Decimal(data.acceleration.y)
+			let az = Decimal(data.acceleration.z)
+			vx += ax * 0.01
+			vy += ay * 0.01
+			vz += az * 0.01
+			
+			
+			let decimalSpeed = pow(vx, 2) + pow(vy, 2)
+			speed += Float(sqrt(Double(truncating: decimalSpeed as NSNumber)))
+			speedValues.append(speed)
+		}
+	}
+	
+	
 }
--- SBRA/ViewController.swift
+++ SBRA/ViewController.swift
@@ -19,10 +19,13 @@ class ViewController: UIViewController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		motionParser.startDataCollection { (data, error) in
+			print(self.motionParser.speedValues.last ?? "no data")
+			/*
 			if let data = data {
-				print(String(format: "x: %.5f, y: %.5f, z: %.5f t:%.1f", abs(data.acceleration.x), abs(data.acceleration.y), abs(data.acceleration.z), data.timestamp))
-				print(String(format: "x: %.2f, y: %.2f, z: %.2f", abs(data.rotationRate.x), abs(data.rotationRate.y), abs(data.rotationRate.z)))
-			}
+				 print(String(format: "x: %.5f, y: %.5f, z: %.5f t:%.1f", abs(data.acceleration.x), abs(data.acceleration.y), abs(data.acceleration.z), data.timestamp))
+				 print(String(format: "x: %.2f, y: %.2f, z: %.2f", abs(data.rotationRate.x), abs(data.rotationRate.y), abs(data.rotationRate.z)))
+			 }
+			*/
 		}
 	}
 
