54b7a8fb86832f007fc09fba49fc456ee8096e25
Wander Siemers <wandersiemers@me.com>
2019-02-04 15:55:27 +0100

    Separated BuildingCategory and VibrationCategory enums out to their own files

---
+++ SBRA/Model/BuildingCategory.swift
@@ -0,0 +1,9 @@
+//
+//  BuildingCategory.swift
+//  SBRA
+//
+//  Created by Wander Siemers on 04/02/2019.
+//  Copyright © 2019 Wander Siemers. All rights reserved.
+//
+
+import Foundation
---
+++ SBRA/Model/VibrationCategory.swift
@@ -0,0 +1,9 @@
+//
+//  VibrationCategory.swift
+//  SBRA
+//
+//  Created by Wander Siemers on 04/02/2019.
+//  Copyright © 2019 Wander Siemers. All rights reserved.
+//
+
+import Foundation
