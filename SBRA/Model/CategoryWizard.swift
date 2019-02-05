a4caa05766a90c05f3f6db7be499d70f4842e3ea
Wander Siemers <wandersiemers@me.com>
2019-02-05 21:34:12 +0100

    Added the questions for vibration category and vibration sensitivity and the links between the questions. Made questions a computed property and removed the extension architecture because that makes the design much simpler.

--- SBRA/Model/CategoryWizard.swift
+++ SBRA/Model/CategoryWizard.swift
@@ -10,52 +10,116 @@
 CategoryWizard manages the graph of questions and the outcomes of the wizard. It can be extended which provide the different wizards available.
 */
 struct CategoryWizard {
-	var questions = [WizardQuestion]()
-}
-
-extension CategoryWizard {
-	private mutating func setupQuestions() {
-		questions = [
-			"Bestaat het gebouw waarin u wilt meten uit een betonnen bebouwing met metselwerk of gevelbekleding?\nVeel bebouwing na 1970 valt in deze categorie",
-			"Betreft de bebouwing een monument of beschermd stadsgezicht?",
-			"Bestaat het pand waarin u wilt meten uit gemetselde wanden?\nVeel bebouwing van voor 1970 valt in deze categorie",
-			"Betreft de bebouwing een monument of beschermd stadsgezicht?",
-			"Verkeert het pand in slechte bouwkundige staat?\nZijn er bijvoorbeeld scheuren of kieren of andere schade aan het pand aanwezig",
-			"Bestaat het pand uit een stalen skelet, glas als gevelbekleding, of hout?"
-		].map { (s: String) -> [Substring] in
-			return s.split(separator: "\n")
-		}.map { (s: [Substring]) -> WizardQuestion in
-			var secondary: String? = nil
-			if s.count > 1 {
-				secondary = String(s[1])
+	
+	var questions: [WizardQuestion] {
+		get {
+			let buildingCategoryQuestions = [
+				"Bestaat het gebouw waarin u wilt meten uit een betonnen bebouwing met metselwerk of gevelbekleding?\nVeel bebouwing na 1970 valt in deze categorie",
+				"Betreft de bebouwing een monument of beschermd stadsgezicht?",
+				"Bestaat het pand waarin u wilt meten uit gemetselde wanden?\nVeel bebouwing van voor 1970 valt in deze categorie",
+				"Verkeert het pand in slechte bouwkundige staat?\nZijn er bijvoorbeeld scheuren of kieren of andere schade aan het pand aanwezig",
+				"Betreft de bebouwing een monument of beschermd stadsgezicht?",
+				"Bestaat het pand uit een stalen skelet, glas als gevelbekleding, of hout?\nBijvoorbeeld houten schuurtjes of kantoorgebouwen met grote glazen gevels"
+				].map { (s: String) -> [Substring] in
+					return s.split(separator: "\n")
+				}.map { (s: [Substring]) -> WizardQuestion in
+					var secondary: String? = nil
+					if s.count > 1 {
+						secondary = String(s[1])
+					}
+					return WizardQuestion(text: String(s[0]), secondaryText: secondary)
 			}
-			return WizardQuestion(text: String(s[0]), secondaryText: secondary, positiveNext: nil, negativeNext: nil)
+			
+			let buildingCategoryOutcomes = [
+				"Het pand behoort tot categorie 1 van de SBR-A richtlijn",
+				"Het pand behoort tot categorie 2 van de SBR-A richtlijn",
+				"Het pand behoort tot categorie 3 van de SBR-A richtlijn"
+			].map({WizardOutcome(text: $0)})
+			
+			buildingCategoryOutcomes[0].buildingCategory = BuildingCategory.category1
+			buildingCategoryOutcomes[1].buildingCategory = BuildingCategory.category2
+			buildingCategoryOutcomes[2].buildingCategory = BuildingCategory.category3
+
+			
+			let buildingCategoryFailureOutcomes = [
+				"Er is geen kwalificatie voor het type bebouwing kunnen maken",
+				"Het pand kan niet worden ingedeeld in een categorie van de SBR-A richtlijn"
+			].map({WizardOutcome(text: $0, isPositive: false)})
+			
+			let vibrationCategoryQuestions = [
+				"Zijn de trillingen die u wilt meten afkomstig van één van de onderstaande bronnen:\n- Explosies\n- Botsingen\n- Het van hoogte legen van kraanbakken\n- Het met de kraan breken van beton\n- Storten van puin in een container(bak)",
+				"Zijn de trillingen die u wilt meten afkomstig van één van de onderstaande bronnen:\n- Weg- of railverkeer\n- Heiwerkzaamheden\n- Het rijden van shovel en kranen",
+				"Zijn de trillingen die u wilt meten afkomstig van één van de onderstaande bronnen:\n- Intrillen van damwanden\n- Trilwalsen en trilplaten"
+				].map({WizardQuestion(text: $0, secondaryText: nil)})
+			
+			let vibrationCategoryOutcomes = [
+				"De trillingen zijn conform de SBR-A richtlijn geclassificeerd als \"kortdurende trillingen\"",
+				"De trillingen zijn conform de SBR-A richtlijn geclassificeerd als \"herhaald kortdurende trillingen\"",
+				"De trillingen zijn conform de SBR-A richtlijn geclassificeerd als \"continue trillingen\""
+				].map({WizardOutcome(text: $0)})
+			
+			vibrationCategoryOutcomes[0].vibrationCategory = .shortDuration
+			vibrationCategoryOutcomes[1].vibrationCategory = .repeatedShortDuration
+			vibrationCategoryOutcomes[2].vibrationCategory = .continuous
+			
+			let sensitiveToVibrationsCategoryQuestions = [
+				"Is het pand gefundeerd op houten funderingspalen of is het gebouw op staal gefundeerd?"
+			].map({WizardQuestion(text: $0, secondaryText: nil)})
+			
+			let sensitiveToVibrationsCategoryOutcomes = [
+				"Het pand is geclassificeerd als trillingsgevoelig conform de SBR-A richtlijn",
+				"Het pand is geclassificeerd als niet trillingsgevoelig conform de SBR-A richtlijn"
+			].map({WizardOutcome(text: $0)})
+			
+			sensitiveToVibrationsCategoryQuestions[0].positiveNext = sensitiveToVibrationsCategoryOutcomes[0]
+			sensitiveToVibrationsCategoryQuestions[0].negativeNext = sensitiveToVibrationsCategoryOutcomes[1]
+			
+			sensitiveToVibrationsCategoryOutcomes[0].sensitiveToVibrations = true
+			sensitiveToVibrationsCategoryOutcomes[1].sensitiveToVibrations = false
+			
+			
+			buildingCategoryQuestions[0].positiveNext = buildingCategoryQuestions[1]
+			buildingCategoryQuestions[0].negativeNext = buildingCategoryQuestions[2]
+			buildingCategoryQuestions[1].positiveNext = buildingCategoryOutcomes[2]
+			buildingCategoryQuestions[1].negativeNext = buildingCategoryOutcomes[0]
+			buildingCategoryQuestions[2].positiveNext = buildingCategoryQuestions[3]
+			buildingCategoryQuestions[2].negativeNext = buildingCategoryQuestions[5]
+			buildingCategoryQuestions[4].positiveNext = buildingCategoryOutcomes[2]
+			buildingCategoryQuestions[4].negativeNext = buildingCategoryOutcomes[1]
+			buildingCategoryQuestions[3].positiveNext = buildingCategoryOutcomes[2]
+			buildingCategoryQuestions[3].negativeNext = buildingCategoryQuestions[4]
+			buildingCategoryQuestions[5].positiveNext = buildingCategoryFailureOutcomes[1]
+			buildingCategoryQuestions[5].negativeNext = buildingCategoryFailureOutcomes[0]
+			
+			/*
+			-- Useful for testing the flow --
+			for element in buildingCategoryQuestions {
+				print("\(element.text) yes -> \(element.positiveNext?.text ?? nil)")
+				print("\(element.text) no -> \(element.negativeNext?.text ?? nil)")
+			}*/
+			
+			buildingCategoryOutcomes[0].next = vibrationCategoryQuestions[0]
+			buildingCategoryOutcomes[1].next = vibrationCategoryQuestions[0]
+			buildingCategoryOutcomes[2].next = vibrationCategoryQuestions[0]
+			
+			
+			vibrationCategoryQuestions[0].positiveNext = vibrationCategoryOutcomes[0]
+			vibrationCategoryQuestions[0].negativeNext = vibrationCategoryQuestions[1]
+			vibrationCategoryQuestions[1].positiveNext = vibrationCategoryOutcomes[1]
+			vibrationCategoryQuestions[1].negativeNext = vibrationCategoryQuestions[2]
+			vibrationCategoryQuestions[2].positiveNext = vibrationCategoryOutcomes[2]
+			vibrationCategoryQuestions[2].negativeNext = WizardOutcome(text: "Er is geen kwalificatie van het type trilling kunnen maken",
+																	   isPositive: false)
+			
+			vibrationCategoryOutcomes[0].next = sensitiveToVibrationsCategoryQuestions[0]
+			vibrationCategoryOutcomes[1].next = sensitiveToVibrationsCategoryQuestions[0]
+			vibrationCategoryOutcomes[2].next = sensitiveToVibrationsCategoryQuestions[0]
+			
+			
+			return buildingCategoryQuestions
 		}
-		
-		let outcomes = [
-			"Het pand behoort tot categorie 1 van de SBR-A richtlijn",
-			"Het pand behoort tot categorie 2 van de SBR-A richtlijn",
-			"Het pand behoort tot categorie 3 van de SBR-A richtlijn"
-		].map({WizardOutcome(text: $0)})
-		
-		let failureOutcomes = [
-			"Er is geen kwalificatie voor het type bebouwing kunnen maken",
-			"Het pand kan niet worden ingedeeld in een categorie van de SBR-A richtlijn"
-		].map({WizardOutcome(text: $0, isPositive: false)})
-		
-		questions[0].positiveNext = questions[1]
-		questions[0].negativeNext = questions[2]
-		questions[1].positiveNext = outcomes[2]
-		questions[1].negativeNext = outcomes[0]
-		questions[2].positiveNext = questions[3]
-		questions[2].negativeNext = questions[5]
-		questions[3].positiveNext = outcomes[2]
-		questions[3].negativeNext = questions[4]
-		questions[4].positiveNext = outcomes[2]
-		questions[4].negativeNext = outcomes[1]
-		questions[5].positiveNext = failureOutcomes[1]
-		questions[5].negativeNext = failureOutcomes[0]
 	}
 }
+
 
 
