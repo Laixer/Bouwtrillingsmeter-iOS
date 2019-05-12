5b0e8b29fb617ce87265f7e514a4d91ae8d162a2
Wander Siemers <wandersiemers@me.com>
2019-05-12 22:21:14 +0200

    Added PowerLimit.swift which provides the limit values for the measurements

---
+++ SBRA/Model/PowerLimit.swift
@@ -0,0 +1,118 @@
+//
+//  PowerLimit.swift
+//  SBRA
+//
+//  Created by Wander Siemers on 12/05/2019.
+//  Copyright © 2019 Wander Siemers. All rights reserved.
+//
+
+import Foundation
+
+struct LimitPoint {
+	var xValue: Float
+	var yValue: Float
+	
+	init(xValue: Float, yValue: Float) {
+		self.xValue = xValue
+		self.yValue = yValue
+	}
+}
+
+private enum YVFactor {
+	case indicative
+	case limited
+	case vibrationSensitive
+	
+	var value: Float {
+		switch self {
+		case .indicative: return 1.6
+		case .limited: return 1.4
+		case .vibrationSensitive: return 1.0
+		}
+		
+	}
+	
+	init?(from measurementSettings: MeasurementSettings) {
+		if let sensitive = measurementSettings.isSensitiveToVibrations {
+			if sensitive == true {
+				self = .vibrationSensitive
+			}
+		}
+		
+		return nil
+	}
+}
+
+private enum YTFactor {
+	case shortVibration
+	case shortVibrationRepeated
+	case continousVibration
+	case vibrationSensitive
+	
+	var value: Float {
+		switch self {
+		case .shortVibration: return 1.0
+		case .shortVibrationRepeated: return 1.5
+		case .continousVibration: return 2.5
+		case .vibrationSensitive: return 1.0
+		}
+	}
+}
+
+struct PowerLimit {
+	
+	func limitForSettings(settings: MeasurementSettings) -> [LimitPoint] {
+		let amplitudes = amplitudesForSettings(settings: settings)
+		
+		var limits = [LimitPoint]()
+		let yvFactor = YVFactor(from: settings)
+		
+		for (index, value) in frequencyValues.enumerated() {
+			if let amplitude = amplitudes?[index], let yvFactor = yvFactor {
+				limits.append(LimitPoint(xValue: value, yValue: amplitude * yvFactor.value))
+			} else {
+				fatalError("can't find amplitude value")
+			}
+		}
+		
+		return limits
+	}
+	
+	private let frequencyValues: [Float] = [0, 10, 50, 100]
+	
+	private let shortVibrationAmplitudes: [[Float]] = [[20, 20, 40, 50],
+												   [5, 5, 15, 20],
+												   [3, 3, 8, 10]]
+	
+	private let repeatedShortVibrationAmplitudes: [[Float]] = [[(13.33333), (13.33333), (26.66666), (33.33333)],
+														   [(3.33333), (3.33333), 10, (13.33333)],
+														   [2, 2, 5.33333, (6.66666)]]
+	
+	private let continuousVibrationAmplitudes: [[Float]] = [[10, 10, 20, 25],
+														[2.5, 2.5, 7.5, 10],
+														[1.5, 1.5, 4, 5]]
+	
+	private func amplitudesForSettings(settings: MeasurementSettings) -> [Float]? {
+		var index: Int?
+		if let buildingCategory = settings.buildingCategory {
+			switch buildingCategory {
+			case .category1: index = 0
+			case .category2: index = 1
+			case .category3: index = 2
+			}
+		}
+		
+		if let index = index {
+			if let vibrationCategory = settings.vibrationCategory {
+				switch vibrationCategory {
+				case .shortDuration: return shortVibrationAmplitudes[index]
+				case .repeatedShortDuration: return repeatedShortVibrationAmplitudes[index]
+				case .continuous: return continuousVibrationAmplitudes[index]
+				}
+				
+			}
+		}
+		
+		return nil
+	}
+}
