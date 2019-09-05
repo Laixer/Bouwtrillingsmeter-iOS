ded1aac2542272e19bb6ee9bf9452611a4e7fc88
James <jamesb@live.nl>
2019-09-05 14:25:26 +0200

    added file with constants

---
+++ SBRA/Const.swift
@@ -0,0 +1,23 @@
+//
+//  Const.swift
+//  SBRA
+//
+//  Created by Anonymous on 04-09-19.
+//  Copyright © 2019 James Bal. All rights reserved.
+//
+
+//swiftlint:disable identifier_name
+
+import Foundation
+
+class Const {
+    
+    static let WAITING_TIME: Int64 = 1000
+    
+    // graphs
+    static let GRAPH_X_COLOR = "#ff0000"
+    static let GRAPH_Y_COLOR = "#0000ff"
+    static let GRAPH_Z_COLOR = "#00ff00"
+    static let GRAPH_CONSTANT_COLOR = "#061557"
+    
+}
