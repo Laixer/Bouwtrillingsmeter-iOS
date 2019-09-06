f5a0795800e1299eb170c036dfa68c0560ab6327
James <jamesb@live.nl>
2019-09-06 15:44:38 +0200

    added extra screen

--- SBRA/Controller/MeasureViewController.swift
+++ SBRA/Controller/MeasureViewController.swift
@@ -32,6 +32,9 @@ class MeasureViewController: UIViewController {
     private var placemark: CLPlacemark?
     
     private let locationManager = CLLocationManager()
+    
+    private let cView = UIView()
+    private let containerView = UIView()
 	
 	init(settings: MeasurementSettings?) {
         
@@ -39,12 +42,8 @@ class MeasureViewController: UIViewController {
 		motionDataParser.settings = settings
 		
 		super.init(nibName: nil, bundle: nil)
-		
-		let saveButton = UIBarButtonItem(title: "Sla op", style: .done, target: self, action: #selector(tappedSaveButton))
-		
-		navigationItem.title = "Meting"
-		
-		navigationItem.rightBarButtonItem = saveButton
+        
+        navigationItem.title = "Meting"
         
 	}
 	
@@ -57,84 +56,125 @@ class MeasureViewController: UIViewController {
 	}
     
     func setupGraphs() {
-        
-        let cover = UIView()
-        cover.backgroundColor = UIColor.white
-        cover.tag = 100
 
         graphPageViewController = GraphPageViewController()
         graphPageViewController!.motionDataParser.settings = settings
         graphPageViewController!.motionDataParser.graphPageView = graphPageViewController!
         
-        tempView = graphPageViewController!.view
-        
-        view.addSubview(tempView!)
-        view.addSubview(cover)
-        
-        tempView!.translatesAutoresizingMaskIntoConstraints = false
-        cover.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            tempView!.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1.0),
-            tempView!.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            tempView!.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            tempView!.rightAnchor.constraint(equalTo: view.rightAnchor),
-            
-            cover.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1.0),
-            cover.leftAnchor.constraint(equalTo: view.leftAnchor),
-            cover.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            cover.rightAnchor.constraint(equalTo: view.rightAnchor)
-        ])
+        motionDataParser.startDataCollection(updateInterval: 0.01)
         
     }
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-        
-        print(UserDefaults.standard.bool(forKey: "graphs"))
 		
 		view.backgroundColor = UIColor.white
         
-        if showingGraphs {
-            setupGraphs()
-        }
-        setupMeasuringLabel()
+        setupDevice()
         
         // get newest location possible
         getLocation()
         
 	}
+    
+    private func setupDevice() {
+        
+        containerView.backgroundColor = UIColor.white
+        
+        let setMobileReady = UILabel()
+        setMobileReady.text = "Leg uw telefoon plat neer"
+        
+        view.addSubview(containerView)
+        containerView.addSubview(setMobileReady)
+        containerView.addSubview(indicator)
+        indicator.startAnimating()
+        
+        let startMeasuring = UIButton()
+        startMeasuring.tintColor = .black
+        startMeasuring.setTitleColor(UIColor.black, for: .normal)
+        startMeasuring.layer.borderWidth = 1
+        startMeasuring.setTitle("Begin met meten", for: .normal)
+        startMeasuring.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        containerView.addSubview(startMeasuring)
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        setMobileReady.translatesAutoresizingMaskIntoConstraints = false
+        startMeasuring.translatesAutoresizingMaskIntoConstraints = false
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addConstraints([
+            
+            containerView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            containerView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            containerView.topAnchor.constraint(equalTo: view.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            setMobileReady.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            setMobileReady.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            
+            startMeasuring.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            startMeasuring.topAnchor.constraint(equalTo: setMobileReady.bottomAnchor, constant: 10),
+            startMeasuring.heightAnchor.constraint(equalToConstant: 48.0),
+            startMeasuring.widthAnchor.constraint(equalToConstant: 210.0),
+            
+            indicator.centerYAnchor.constraint(equalTo: setMobileReady.centerYAnchor),
+            indicator.leftAnchor.constraint(equalToSystemSpacingAfter: setMobileReady.rightAnchor, multiplier: 1.0),
+            
+        ])
+        
+        startMeasuring.addTarget(self, action: #selector(startMeasurement), for: .touchUpInside)
+        
+    }
+    
+    @objc private func startMeasurement() {
+        containerView.removeFromSuperview()
+        if showingGraphs {
+            setupGraphs()
+        }
+        setupMeasuringLabel()
+        
+        let saveButton = UIBarButtonItem(title: "Sla op", style: .done, target: self, action: #selector(tappedSaveButton))
+        
+        navigationItem.rightBarButtonItem = saveButton
+    }
 	
 	private func setupMeasuringLabel() {
         
+        view.addSubview(cView)
+        cView.backgroundColor = UIColor.white
+        
         measuringLabel.text = "Aan het meten..."
-        measuringLabel.tag = 101
-        view.addSubview(measuringLabel)
+        cView.addSubview(measuringLabel)
 		
-        indicator.tag = 101
-		view.addSubview(indicator)
+		cView.addSubview(indicator)
 		indicator.startAnimating()
         
         let showGraph = UIButton()
-        showGraph.tag = 101
         showGraph.tintColor = .black
         showGraph.setTitleColor(UIColor.black, for: .normal)
         showGraph.layer.borderWidth = 1
         showGraph.setTitle("Toon grafieken", for: .normal)
         showGraph.titleLabel?.font = UIFont.boldSystemFont(ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize)
         if showingGraphs {
-            view.addSubview(showGraph)
+            cView.addSubview(showGraph)
         }
         
         showGraph.addTarget(self, action: #selector(clickedShowGraphs), for: .touchUpInside)
         
+        cView.translatesAutoresizingMaskIntoConstraints = false
 		measuringLabel.translatesAutoresizingMaskIntoConstraints = false
 		indicator.translatesAutoresizingMaskIntoConstraints = false
         showGraph.translatesAutoresizingMaskIntoConstraints = false
 		
 		view.addConstraints([
-			measuringLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			measuringLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            cView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            cView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            cView.topAnchor.constraint(equalTo: view.topAnchor),
+            cView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+			measuringLabel.centerXAnchor.constraint(equalTo: cView.centerXAnchor),
+			measuringLabel.centerYAnchor.constraint(equalTo: cView.centerYAnchor),
 			
 			indicator.centerYAnchor.constraint(equalTo: measuringLabel.centerYAnchor),
 			indicator.leftAnchor.constraint(equalToSystemSpacingAfter: measuringLabel.rightAnchor, multiplier: 1.0),
@@ -143,8 +183,8 @@ class MeasureViewController: UIViewController {
         
         if showingGraphs {
             view.addConstraints([
-                showGraph.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                showGraph.topAnchor.constraint(equalTo: measuringLabel.bottomAnchor, constant: 5),
+                showGraph.centerXAnchor.constraint(equalTo: cView.centerXAnchor),
+                showGraph.topAnchor.constraint(equalTo: measuringLabel.bottomAnchor, constant: 10),
                 showGraph.heightAnchor.constraint(equalToConstant: 48.0),
                 showGraph.widthAnchor.constraint(equalToConstant: 210.0),
             ])
@@ -155,12 +195,9 @@ class MeasureViewController: UIViewController {
     
     //swiftlint:disable identifier_name
     @objc private func clickedShowGraphs() {
-        if let viewWithTag = self.view.viewWithTag(101) {
-            viewWithTag.removeFromSuperview()
-        }
-        if let viewWithTag = self.view.viewWithTag(100) {
-            viewWithTag.removeFromSuperview()
-        }
+        
+        navigationController?.pushViewController(graphPageViewController!, animated: true)
+        
     }
     
     private func getLocation() {
@@ -179,7 +216,8 @@ class MeasureViewController: UIViewController {
     }
 	
 	@objc private func tappedSaveButton() {
-        
+
+        motionDataParser.stopDataCollection()
         DataHandler.stopMeasuring()
         MeasurementControl.onFinishMeasurement(placemark: placemark)
 
@@ -221,7 +259,21 @@ extension MeasureViewController: CLLocationManagerDelegate {
         if status == .authorizedWhenInUse {
             manager.requestLocation()
         } else {
-            print("error: not authorized to retrieve location")
+            let alertController = UIAlertController(title: "Locatievoorziening verplicht", message: "Geef alstublieft toestemming in de settings.", preferredStyle: UIAlertController.Style.alert)
+            
+            let okAction = UIAlertAction(title: "Settings", style: .default, handler: {(cAlertAction) in
+                //Redirect to Settings app
+                UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
+            })
+            
+            let cancelAction = UIAlertAction(title: "Annuleer", style: UIAlertAction.Style.cancel, handler: {(cAlertAcoin) in
+                    self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+            })
+            alertController.addAction(cancelAction)
+            
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
         }
     }
 }
